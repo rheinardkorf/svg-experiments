154f2697014c9be6e43d8b932825e7716a491b96
Rheinard Korf <rheinard@thekorfs.com>
2016-08-05 22:59:46 +1000

    Some more isometric fun.

--- isometric/isometric.js
+++ isometric/isometric.js
@@ -56,12 +56,14 @@ Object.assign( GameGrid, {
 	tileCols: 10,
 	svg: false,
 	map: false,
+	defs: false,
 	svgNS: 'http://www.w3.org/2000/svg',
 	_topPoint: false,
 	_leftPoint: false,
 	_rightPoint: false,
 	_bottomPoint: false,
 	_origin: false,
+	_lastFill: false,
 
 	/**
 	 * Create a document ready listener.
@@ -116,9 +118,13 @@ Object.assign( GameGrid, {
 		}
 		this.log( 'SVG created.' );
 
+		this.defs = document.createElementNS( this.svgNS, 'defs' );
+		this.svg.appendChild( this.defs );
 		this.map = document.createElementNS( this.svgNS, 'g' );
 		this.svg.appendChild( this.map );
 		this.log( 'Map area created.' );
+
+		this.patterns();
 
 		this.surface.appendChild( this.svg );
 
@@ -128,8 +134,50 @@ Object.assign( GameGrid, {
 		this.log( 'Events added.' );
 	},
 
-	draw: function() {
-		var blocks = [];
+	patterns: function() {
+		// <pattern xmlns="http://www.w3.org/2000/svg" id="img1" patternUnits="userSpaceOnUse" width="600" height="450">
+        //     <image xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="daisy-grass-repeating-background.jpg" x="0" y="0" width="600" height="450"/><!-- Image from http://silviahartmann.com/background-tile/6-grass-meadow-tile.php-->
+        // </pattern>
+		this.defs.appendChild( this.createPattern( 'grass1', 'grass1.png', this.width, this.height ) );
+		this.defs.appendChild( this.createPattern( 'grass2', 'grass2.png', this.width, this.height ) );
+		this.defs.appendChild( this.createPattern( 'sheep', 'sheep.png', this.width - 10 , this.width - 10 ) );
+	},
+
+	createPattern: function( id, src, width, height, xt, yt ) {
+		var pattern = document.createElementNS( this.svgNS, 'pattern' ),
+			image = document.createElementNS( this.svgNS, 'image' ),
+			namespace = 'http://www.w3.org/1999/xlink';
+		pattern.setAttribute( 'xmlns', this.svgNS );
+		// pattern.setAttributeNS( null, 'patternUnits', 'userSpaceOnUse' );
+		// pattern.setAttributeNS( null, 'patternUnits', 'objectBoundingBox' );
+		// pattern.setAttributeNS( null, 'patternContentUnits', 'userSpaceOnUse' );
+		pattern.setAttributeNS( null, 'id', id );
+		pattern.setAttributeNS( null, 'width', 1 );
+		pattern.setAttributeNS( null, 'height', 1 );
+		// image.setAttributeNS( null, 'href', src );
+		image.setAttribute( 'xmlns:xlink', namespace );
+		image.setAttributeNS( namespace, 'xlink:href', src );
+		image.setAttributeNS( null, 'x', 0 );
+		image.setAttributeNS( null, 'y', 0 );
+		image.setAttributeNS( null, 'width', width );
+		image.setAttributeNS( null, 'height', width ); // because its square
+		pattern.appendChild( image );
+
+		if ( 'undefined' !== typeof xt && 'undefined' !== typeof yt ) {
+			image.setAttributeNS( null, 'transform', 'translate(' + xt + ' ' + yt + ')' );
+		} else if ( 'undefined' !== typeof xt ) {
+			image.setAttributeNS( null, 'transform', 'translate(' + xt + ' 0)' );
+		} else if ( 'undefined' !== typeof yt ) {
+			image.setAttributeNS( null, 'transform', 'translate( 0 ' + yt + ')' );
+		}
+
+		return pattern;
+	},
+
+	drawMap: function() {
+		var blocks = [],
+			pattern = 'grass1',
+			pos = false;
 
 		for ( y = 0; y < this.tileRows; y++ ) {
 			for ( x = 0; x < this.tileCols; x++ ) {
@@ -138,13 +186,25 @@ Object.assign( GameGrid, {
 				block.setAttributeNS( null, 'y', this.height * y );
 				block.setAttributeNS( null, 'width', this.width );
 				block.setAttributeNS( null, 'height', this.height );
-				block.setAttributeNS( null, 'class', 'tile' );
-				block.setAttributeNS(null, 'style', 'fill:white;stroke:black;stroke-width:1');
+				block.setAttributeNS( null, 'class', 'tile tile' + ( x + 1 ) + '-' + ( y + 1 ) );
+				pattern = 'grass2' === pattern ? 'grass1' : 'grass2';
+				block.setAttributeNS(null, 'style', 'fill:url(\'#' + pattern + '\');stroke:black;stroke-width:1');
 				block.setAttributeNS( null, 'transform', 'rotate(' + this._angle + ') skewX(' + (  -1 * this._angle ) + ')' );
 				this.map.appendChild( block );
+				if( ! pos ) {
+					pos = block.getBoundingClientRect();
+				}
 			}
+			pattern = 'grass2' === pattern ? 'grass1' : 'grass2';
 		}
-		this.log( 'Blocks drawn.' );
+		// Draw Thick Line
+		var block = document.createElementNS(this.svgNS, 'path');
+		block.setAttributeNS(null, 'style', 'fill:none;stroke:black;stroke-width:3');
+		block.setAttributeNS( null, 'd', 'M' + (this.width * this.tileCols + 2 ) + ',0 L' + (this.width * this.tileCols + 2 ) + ',' + ( this.height * this.tileRows + 2 ) + ' 0,' + ( this.height * this.tileRows + 2 ) );
+		block.setAttributeNS( null, 'transform', 'rotate(' + this._angle + ') skewX(' + (  -1 * this._angle ) + ')' );
+		this.map.appendChild( block );
+
+		this.log( 'Map drawn.' );
 
 		/**
 		 * Get SVG width and height;
@@ -168,8 +228,49 @@ Object.assign( GameGrid, {
 		 */
 		this.map.setAttributeNS( null, 'transform', 'translate(' + width / 2 + ' ' + 0 + ')' );
 		this.svg.setAttribute( 'width', width);
-		this.svg.setAttribute( 'height', height);
+		this.svg.setAttribute( 'height', height + 5);
 		this.log( 'Viewport adjusted.');
+	},
+
+	draw: function() {
+		var pos = document.querySelector( '.tile1-2' ).getBoundingClientRect();
+		var block = document.createElementNS(this.svgNS, 'rect');
+		block.setAttributeNS( null, 'x', pos.left + 20 );
+		block.setAttributeNS( null, 'y', pos.top );
+		block.setAttributeNS( null, 'width', this.width );
+		block.setAttributeNS( null, 'height', this.height );
+		block.setAttributeNS(null, 'style', 'fill:url(\'#sheep\');');
+		this.svg.appendChild( block );
+
+		var pos = document.querySelector( '.tile2-2' ).getBoundingClientRect();
+		var block = document.createElementNS(this.svgNS, 'rect');
+		block.setAttributeNS( null, 'x', pos.left + 20 );
+		block.setAttributeNS( null, 'y', pos.top );
+		block.setAttributeNS( null, 'width', this.width );
+		block.setAttributeNS( null, 'height', this.height );
+		block.setAttributeNS(null, 'style', 'fill:url(\'#sheep\');');
+		this.svg.appendChild( block );
+
+		// this.log( pos );
+
+
+		// var block = document.createElementNS(this.svgNS, 'rect');
+		// block.setAttributeNS( null, 'x', this.width * x);
+		// block.setAttributeNS( null, 'y', this.height * y );
+		// block.setAttributeNS( null, 'width', this.width );
+		// block.setAttributeNS( null, 'height', this.height );
+		// block.setAttributeNS( null, 'class', 'tile tile' + ( x + 1 ) + '-' + ( y + 1 ) );
+		// pattern = 'grass2' === pattern ? 'grass1' : 'grass2';
+		// block.setAttributeNS(null, 'style', 'fill:url(\'#' + pattern + '\');stroke:black;stroke-width:1');
+		// block.setAttributeNS( null, 'transform', 'rotate(' + this._angle + ') skewX(' + (  -1 * this._angle ) + ')' );
+		// this.map.appendChild( block );
+	},
+
+	hasClass: function( el, className ) {
+		if (el.classList)
+  			return el.classList.contains(className);
+		else
+  			return new RegExp('(^| )' + className + '( |$)', 'gi').test(el.className);
 	},
 
 	/**
@@ -177,6 +278,7 @@ Object.assign( GameGrid, {
 	 */
 	setup: function ( e ) {
 		GameGrid.init();
+		GameGrid.drawMap();
 		GameGrid.draw();
 	},
 
@@ -186,18 +288,21 @@ Object.assign( GameGrid, {
 
 	dim: function( e ) {
 
-		if ( 'rect' === e.target.nodeName ) {
-			e.target.style.fill = 'red';
+		if ( 'rect' === e.target.nodeName && GameGrid.hasClass(e.target, 'tile' ) ) {
+			// this._lastFill = e.target.style.fill;
+			// e.target.style.fill = 'red';
 			e.target.style.opacity = '0.5';
 		}
 	},
 
 	restore: function( e ) {
-		if ( 'rect' === e.target.nodeName ) {
-			e.target.style.fill = 'white';
+		if ( 'rect' === e.target.nodeName && GameGrid.hasClass(e.target, 'tile' ) ) {
+			// e.target.style.fill = this._lastFill;
+			// e.target.style.background = 'url("./grass1.png")';
 			e.target.style.opacity = '1.0';
 		}
 	}
+
 } );
 
 GameGrid.log('Document is ready!').ready( GameGrid.setup, document.body, 'gamegrid' );
